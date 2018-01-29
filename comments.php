b50317a7ecc4282e8989b781674efa8e7a3f01fb
imath <math.viet@gmail.com>
2018-01-29 21:59:53 +0100

    Improve comments display

--- comments.php
+++ comments.php
@@ -21,52 +21,53 @@ if ( post_password_required() ) {
 ?>
 
 <div id="comments" class="comments-area">
-
-	<?php
-	// You can start editing here -- including this comment!
-	if ( have_comments() ) : ?>
-		<h2 class="comments-title">
-			<?php
-			$comment_count = get_comments_number();
-			if ( 1 === $comment_count ) {
-				printf(
-					/* translators: 1: title. */
-					esc_html_e( 'One thought on &ldquo;%1$s&rdquo;', 'theme' ),
-					'<span>' . get_the_title() . '</span>'
-				);
-			} else {
-				printf( // WPCS: XSS OK.
-					/* translators: 1: comment count number, 2: title. */
-					esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $comment_count, 'comments title', 'theme' ) ),
-					number_format_i18n( $comment_count ),
-					'<span>' . get_the_title() . '</span>'
-				);
-			}
-			?>
-		</h2><!-- .comments-title -->
-
-		<?php the_comments_navigation(); ?>
-
-		<ol class="comment-list">
-			<?php
-				wp_list_comments( array(
-					'style'      => 'ol',
-					'short_ping' => true,
-				) );
-			?>
-		</ol><!-- .comment-list -->
-
-		<?php the_comments_navigation();
-
-		// If comments are closed and there are comments, let's leave a little note, shall we?
-		if ( ! comments_open() ) : ?>
-			<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'theme' ); ?></p>
+	<div class="wrap">
 		<?php
-		endif;
+		// You can start editing here -- including this comment!
+		if ( have_comments() ) : ?>
+			<h2 class="comments-title">
+				<?php
+				$comment_count = get_comments_number();
+				if ( 1 === $comment_count ) {
+					printf(
+						/* translators: 1: title. */
+						esc_html_e( 'Un commentaire sur &ldquo;%1$s&rdquo;', 'theme' ),
+						'<span>' . get_the_title() . '</span>'
+					);
+				} else {
+					printf( // WPCS: XSS OK.
+						/* translators: 1: comment count number, 2: title. */
+						esc_html( _nx( '%1$s commentaire sur &ldquo;%2$s&rdquo;', '%1$s commentaires sur &ldquo;%2$s&rdquo;', $comment_count, 'comments title', 'theme' ) ),
+						number_format_i18n( $comment_count ),
+						'<span>' . get_the_title() . '</span>'
+					);
+				}
+				?>
+			</h2><!-- .comments-title -->
 
-	endif; // Check for have_comments().
+			<?php the_comments_navigation(); ?>
 
-	comment_form();
-	?>
+			<ol class="comment-list">
+				<?php
+					wp_list_comments( array(
+						'avatar_size' => 100,
+						'style'       => 'ol',
+						'short_ping'  => true,
+					) );
+				?>
+			</ol><!-- .comment-list -->
 
+			<?php the_comments_navigation();
+
+			// If comments are closed and there are comments, let's leave a little note, shall we?
+			if ( ! comments_open() ) : ?>
+				<p class="no-comments"><?php esc_html_e( 'Les commentaires sont fermés.', 'theme' ); ?></p>
+			<?php
+			endif;
+
+		endif; // Check for have_comments().
+
+		comment_form();
+		?>
+	</div><!-- .wrap -->
 </div><!-- #comments -->
